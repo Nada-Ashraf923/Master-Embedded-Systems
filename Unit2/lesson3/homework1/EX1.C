0206ef26669215a3c837b7ec5e4455319a503e2d
Nada Ashraf Mohamed <nadaashraf9232@gmail.com>
2023-09-04 17:30:14 +0300

    Unit2

---
+++ Unit2/lesson3/homework1/EX1.C
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : firstproject.c
+ Author      : Nada Ashraf
+ Version     :
+ Copyright   : Your copyright notice
+ Description : c basic
+ ============================================================================
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+//EX1
+void  main (void)
+{
+
+	printf("C Programming");
+
+}
